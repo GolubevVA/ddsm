137bb3eaf01276dfe3141a773e3cba72da45d446
GolubevVA <v.golubev2004@gmail.com>
2026-02-04 13:57:19 +0300

    add input_path field

--- external/selene_utils.py
+++ external/selene_utils.py
@@ -89,6 +89,7 @@ class MemmapGenome(Genome):
         super().__init__(
             input_path, blacklist_regions=blacklist_regions, bases_order=bases_order,
         )
+        self.input_path = input_path  # Store input_path for later use
         self.memmapfile = memmapfile
         self.initialized = False  # Add this line to fix pickling issue
         if init_unpicklable:
